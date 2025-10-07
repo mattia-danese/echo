930a725783700cede6419b3606188577ae591698
Mattia Danese <themattiadanese@gmail.com>
2025-10-07 00:53:35 -0400

    biome src/lib

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,17 +1,17 @@
-import { isValidPhoneNumber } from 'libphonenumber-js';
-import { Filter } from 'bad-words';
+import { Filter } from "bad-words";
+import { isValidPhoneNumber } from "libphonenumber-js";
 
 const filter = new Filter();
 
 export const isPhoneNumberValid = (phone: string): boolean => {
-    return isValidPhoneNumber(phone, { defaultCountry: 'US' });
-}
+  return isValidPhoneNumber(phone, { defaultCountry: "US" });
+};
 
 export const isNameValid = (name: string): boolean => {
-    return (
-        /^(?=.*\p{L})[\p{L}\p{M}’'\- ]+$/u.test(name.trim()) &&
-        name.length >= 1 &&
-        name.length <= 50 &&
-        !filter.isProfane(name.toLowerCase())
-      );
-}
+  return (
+    /^(?=.*\p{L})[\p{L}\p{M}’'\- ]+$/u.test(name.trim()) &&
+    name.length >= 1 &&
+    name.length <= 50 &&
+    !filter.isProfane(name.toLowerCase())
+  );
+};
