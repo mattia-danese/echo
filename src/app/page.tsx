c2c3b9cc8ffcada25eaa7d9acd7864e01f504b64
Mattia Danese <themattiadanese@gmail.com>
2025-10-07 00:38:21 -0400

    biome app/page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import LogInSignUp from "@/components/LogInSignUp";
 
 interface PageProps {
-    searchParams: Promise<{
-        accountStatus?: string;
-    }>;
+  searchParams: Promise<{
+    accountStatus?: string;
+  }>;
 }
 
 export default async function FriendRequestPage({ searchParams }: PageProps) {
-    const resolvedSearchParams = await searchParams;
-    const accountStatus = resolvedSearchParams.accountStatus;
+  const resolvedSearchParams = await searchParams;
+  const accountStatus = resolvedSearchParams.accountStatus;
 
   return <LogInSignUp accountStatus={accountStatus} />;
 }
